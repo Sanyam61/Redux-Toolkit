2e5450db451ec08d41e360eb500bc8a9ca15c563
ParikhSanyam <sanyamparikh435@gmail.com>
2023-07-11 08:49:02 +0530

    Redux Toolkit Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {useSelector, useDispatch} from "react-redux";
+import { useEffect } from "react";
+import { fetchContent } from "./contentSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContent());
+  }, [dispatch]);
+
+  const contents = useSelector((state) => state.content.contents);
+  const isLoading = useSelector((state) => state.content.isLoading);
+  const error = useSelector((state) => state.content.error);
+
+  if (isLoading) {
+    return "loading...";
+  }
+
+  if (error) {
+    return error;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid gap-4 grid-cols-2  md:grid-cols-4 lg:grid-cols-8  p-4">
+      {contents.map((content) => (
+        <div key={content.id}>
+          <img
+            src={`${content.thumbnailUrl}`}
+            alt={`${content.title}`}
+            className="w-full h-full rounded"
+          />
+        </div>
+      ))}
     </div>
   );
 }
